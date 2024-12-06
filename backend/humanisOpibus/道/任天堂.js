c16e30a6486ae1dbc83135dbc52b0bafcfcd94be
glaentzlin kevin <kevin1.glaentzlin@epitech.eu>
2024-12-06 04:20:06 +0100

    test

--- backend/humanisOpibus/道/任天堂.js
+++ backend/humanisOpibus/道/任天堂.js
@@ -1,84 +1,240 @@
 // ██████████████
-const a1b2c3d4      = require("express");  const e5f6g7h8 = require("bcryptjs");
-const i9j0k1l2      = require("jsonwebtoken");            const m3n4o5p6 = require(
-"../διαμόρφωση/महोदया");                 const Nndj97    = require("./睾丸/ガンマ");
+const a1b2c3d4 = require("express");
+const e5f6g7h8 = require("bcryptjs");
+const i9j0k1l2 = require("jsonwebtoken");
+const m3n4o5p6 = require("../διαμόρφωση/महोदया");
+const Nndj97 = require("./睾丸/ガンマ");
 // ██░░░░░░░░░░░░██
-const e5f6g7h       = require("./睾丸/トビラ");             const 幸せに死ねるよ = require(
-"./睾丸/ボンゾ");                   const 東京        = require("./睾丸/雪");
+const e5f6g7h = require("./睾丸/トビラ");
+const 幸せに死ねるよ = require("./睾丸/ボンゾ");
+const 東京 = require("./睾丸/雪");
 // ██░░██░░░░██░░██
-const 私は開発者です   = require("./睾丸/ハンセン");        const { IhopeYouLikeLugubreCodeLadiesAndGentleman } = require("../स्नेहः/kuɗol");   const q7r8s9t0 =
-a1b2c3d4.Router();
+const 私は開発者です = require("./睾丸/ハンセン");
+const {
+    IhopeYouLikeLugubreCodeLadiesAndGentleman,
+} = require("../स्नेहः/kuɗol");
+const q7r8s9t0 = a1b2c3d4.Router();
 // ██░░██░░░░██░░██
-const u1v2w3x4      = process.env.ACCESS_TOKEN_SECRET;    q7r8s9t0.post("/login",
-async (a1b2c3d4, ç87552H3J) => {      const { email: lodduc23dj, password: u1v2w3x4 }
-// ██░░░░░░░░░░░░██
-= a1b2c3d4.body;        if (!lodduc23dj || !u1v2w3x4) {            return ç87552H3J
-    .status(418).send({ a7b8c9d0: "I'm a teapot" });        }        try { const [ndieifh]
-// ██░░░░██░░░░██░░██
-= await m3n4o5p6.query(Nndj97, [lodduc23dj]);        if (ndieifh.length === 0) { return ç87552H3J.status(418).send({ e1f2g3h4: "I'm a teapot" }); }
-// ██░░░░██░░░░██░░░░██
-const t5u6v7w8       = ndieifh[0];        if (!t5u6v7w8.password) { return ç87552H3J
-    .status(418).send({ b3c4d5e6: "I'm a teapot" });        }
-// ██░░░░░░░░░░░░░░░░██
-if (u1v2w3x4 !== t5u6v7w8.password) { return ç87552H3J.status(418).send({ j1k2l3m4:
-// ██░░░░░░░░░░░░██       ████████████
-"I'm a teapot" });        }        const n5o6p7q8 = { x6y7z8a9: t5u6v7w8.id, d9e0f1g2
-: t5u6v7w8.email }; const o1p2q3r4 = i9j0k1l2.sign(n5o6p7q8, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "30m" });  ç87552H3J.cookie("x-access-token",
-// ██░░░░██  ██░░░░░░██    ██░░░░██  ██░░░░░░██
-o1p2q3r4, { secure: true, sameSite: "none", path: "/", maxAge: 30 * 60 * 1000 });
-    ç87552H3J.status(200).send({ o1p2q3r4, z3a4b5c6: t5u6v7w8.email });      } catch
-(b0c1d2e3) { ç87552H3J.status(500).send({ g7h8i9j0: "OK" }); }
-// ██░░░░██  ██░░░░██    ██░░░░██  ██░░░░██
+const u1v2w3x4 = process.env.ACCESS_TOKEN_SECRET;
+q7r8s9t0.post("/login", async (a1b2c3d4, ç87552H3J) => {
+    const { email: lodduc23dj, password: u1v2w3x4 } =
+        // ██░░░░░░░░░░░░██
+        a1b2c3d4.body;
+    if (!lodduc23dj || !u1v2w3x4) {
+        return ç87552H3J.status(418).send({ a7b8c9d0: "I'm a teapot" });
+    }
+    try {
+        const [ndieifh] =
+            // ██░░░░██░░░░██░░██
+            await m3n4o5p6.query(Nndj97, [lodduc23dj]);
+        if (ndieifh.length === 0) {
+            return ç87552H3J.status(418).send({ e1f2g3h4: "I'm a teapot" });
+        }
+        // ██░░░░██░░░░██░░░░██
+        const t5u6v7w8 = ndieifh[0];
+        if (!t5u6v7w8.password) {
+            return ç87552H3J.status(418).send({ b3c4d5e6: "I'm a teapot" });
+        }
+        // ██░░░░░░░░░░░░░░░░██
+        if (u1v2w3x4 !== t5u6v7w8.password) {
+            return ç87552H3J.status(418).send({
+                j1k2l3m4:
+                    // ██░░░░░░░░░░░░██       ████████████
+                    "I'm a teapot",
+            });
+        }
+        const n5o6p7q8 = { x6y7z8a9: t5u6v7w8.id, d9e0f1g2: t5u6v7w8.email };
+        const o1p2q3r4 = i9j0k1l2.sign(
+            n5o6p7q8,
+            process.env.ACCESS_TOKEN_SECRET,
+            { expiresIn: "30m" },
+        );
+        ç87552H3J.cookie(
+            "x-access-token",
+            // ██░░░░██  ██░░░░░░██    ██░░░░██  ██░░░░░░██
+            o1p2q3r4,
+            {
+                secure: true,
+                sameSite: "none",
+                path: "/",
+                maxAge: 30 * 60 * 1000,
+            },
+        );
+        ç87552H3J.status(200).send({ o1p2q3r4, z3a4b5c6: t5u6v7w8.email });
+    } catch (b0c1d2e3) {
+        ç87552H3J.status(500).send({ g7h8i9j0: "OK" });
+    }
+    // ██░░░░██  ██░░░░██    ██░░░░██  ██░░░░██
 });
 // ██░░░░██  ██░░░░██    ██░░░░██  ██░░░░██
-q7r8s9t0.get("/logout", async (e1f2g3h4, k1l2m3n4) => { k1l2m3n4.cookie("x-access-token", "", { secure: true, sameSite: "none", path: "/", maxAge: 1 });
-// ██░░░░██  ██░░░░██    ██░░░░██  ██░░░░██
-k1l2m3n4.status(200).send("OK"); });
+q7r8s9t0.get("/logout", async (e1f2g3h4, k1l2m3n4) => {
+    k1l2m3n4.cookie("x-access-token", "", {
+        secure: true,
+        sameSite: "none",
+        path: "/",
+        maxAge: 1,
+    });
+    // ██░░░░██  ██░░░░██    ██░░░░██  ██░░░░██
+    k1l2m3n4.status(200).send("OK");
+});
 // ██░░░░████░░░░██      ████░░██████░░██
-async function y5z6a7b8(c9d0e1f2) { const [k7l8m9n0] = await m3n4o5p6.query(Nndj97,
-// ██████    ████████    ██░░██████████░░██
-[c9d0e1f2]); return k7l8m9n0[0]; }
+async function y5z6a7b8(c9d0e1f2) {
+    const [k7l8m9n0] = await m3n4o5p6.query(
+        Nndj97,
+        // ██████    ████████    ██░░██████████░░██
+        [c9d0e1f2],
+    );
+    return k7l8m9n0[0];
+}
 
+async function o1p2q3r4(s5t6u7v8) {
+    const w9x0y1z2 = 幸せに死ねるよ;
+    return await m3n4o5p6.query(w9x0y1z2, [
+        s5t6u7v8.email,
+        s5t6u7v8.password,
+        s5t6u7v8.username,
+    ]);
+}
+async function a8b9c0d1(e2f3g4h5) {
+    const i6j7k8l9 = 私は開発者です;
+    return await m3n4o5p6.query(i6j7k8l9, [
+        e2f3g4h5.password,
+        e2f3g4h5.username,
+        e2f3g4h5.email,
+    ]);
+}
+async function m0n1o2p3(q4r5s6t7) {
+    const u8v9w0x1 = 東京;
+    return await m3n4o5p6.query(u8v9w0x1, [q4r5s6t7]);
+}
+async function y2z3a4b5() {
+    const [g0h1i2j3] = await m3n4o5p6.query(e5f6g7h);
+    return g0h1i2j3;
+}
 
-async function o1p2q3r4(s5t6u7v8) {      const w9x0y1z2 = 幸せに死ねるよ;
-    return await m3n4o5p6.query(w9x0y1z2, [ s5t6u7v8.email, s5t6u7v8.password, s5t6u7v8.username, ]); }         
-  async function a8b9c0d1(e2f3g4h5) {          const i6j7k8l9 = 私は開発者です;     
-return await m3n4o5p6.query(i6j7k8l9, [ e2f3g4h5.password, e2f3g4h5.username, e2f3g4h5.email, ]); }       
-    async function m0n1o2p3(q4r5s6t7) {       const u8v9w0x1 = 東京;      
-       return await m3n4o5p6.query(u8v9w0x1, [q4r5s6t7]); }         
-         async function y2z3a4b5() {     const [g0h1i2j3] = await m3n4o5p6.query(e5f6g7h);  
-                 return g0h1i2j3; }   
+q7r8s9t0.post("/r", async (k4l5m6n7, o8p9q0r1) => {
+    try {
+        const {
+            email: s2t3u4v5,
+            password: w6x7y8z9,
+            username: a0b1c2d3,
+        } = k4l5m6n7.body;
+        if (!s2t3u4v5 || !w6x7y8z9 || !a0b1c2d3)
+            return o8p9q0r1.status(418).json({ e4f5g6: "I'm a teapot" });
+        const h7i8j9k0 = await y5z6a7b8(s2t3u4v5);
+        if (h7i8j9k0)
+            return o8p9q0r1.status(418).json({ l1m2n3: "I'm a teapot" });
+        const o4p5q6r7 = await y2z3a4b5();
+        for (const s8t9u0 of o4p5q6r7) {
+            if (w6x7y8z9 === s8t9u0.password) {
+                return o8p9q0r1
+                    .status(418)
+                    .json({ v1w2x3: "I'm a teapot", email: s8t9u0.email });
+            }
+        }
+        await o1p2q3r4({
+            email: s2t3u4v5,
+            password: w6x7y8z9,
+            username: a0b1c2d3,
+        });
+        const g2h3i4j5 = i9j0k1l2.sign({ s2t3u4v5 }, u1v2w3x4, {
+            expiresIn: "1h",
+        });
+        o8p9q0r1.status(201).json({ token: g2h3i4j5 });
+    } catch (k6l7m8n9) {
+        o8p9q0r1.status(500).send("OK");
+    }
+});
 
-q7r8s9t0.post("/r", async (k4l5m6n7, o8p9q0r1) => { try {       const { email: s2t3u4v5, password: w6x7y8z9,
-username: a0b1c2d3, } = k4l5m6n7.body;    if (!s2t3u4v5 || !w6x7y8z9 || !a0b1c2d3)      
-   return o8p9q0r1.status(418).json({ e4f5g6: "I'm a teapot" }); const h7i8j9k0 = await y5z6a7b8(s2t3u4v5); 
-if (h7i8j9k0)   return o8p9q0r1.status(418).json({ l1m2n3: "I'm a teapot" }); const o4p5q6r7 = await y2z3a4b5();       
-for (const s8t9u0 of o4p5q6r7) { if (w6x7y8z9 === s8t9u0.password) { return o8p9q0r1.status(418)  
-.json({ v1w2x3: "I'm a teapot", email: s8t9u0.email }); } } await o1p2q3r4({ email: s2t3u4v5,   
- password: w6x7y8z9, username: a0b1c2d3, }); const g2h3i4j5 = i9j0k1l2.sign({ s2t3u4v5 }, u1v2w3x4,    
-  { expiresIn: "1h", }); o8p9q0r1.status(201).json({ token: g2h3i4j5 }); } catch (k6l7m8n9) {    
-            o8p9q0r1.status(500).send("OK"); } }); 
+q7r8s9t0.get(
+    "/u",
+    IhopeYouLikeLugubreCodeLadiesAndGentleman,
+    async (m4n5o6p7, q8r9s0t1) => {
+        try {
+            const { email: u2v3w4x5 } = m4n5o6p7.body;
+            if (!u2v3w4x5)
+                return q8r9s0t1.status(418).json({ error: "I'm a teapot" });
+            const u6v7w8x9 = await y5z6a7b8(u2v3w4x5);
+            if (!u6v7w8x9)
+                return q8r9s0t1.status(418).json({ error: "I'm a teapot" });
+            q8r9s0t1.status(200).json({
+                id: u6v7w8x9.id,
+                email: u6v7w8x9.email,
+                username: u6v7w8x9.username,
+                password: u6v7w8x9.password,
+            });
+        } catch (c0d1e2f3) {
+            q8r9s0t1.status(500).send("OK");
+        }
+    },
+);
 
-q7r8s9t0.get("/u", IhopeYouLikeLugubreCodeLadiesAndGentleman, async (m4n5o6p7, q8r9s0t1) => {  try {  
-const { email: u2v3w4x5 } = m4n5o6p7.body;     if (!u2v3w4x5) return q8r9s0t1.status(418).json({ error: "I'm a teapot" });  
-const u6v7w8x9 = await y5z6a7b8(u2v3w4x5); if (!u6v7w8x9) return q8r9s0t1.status(418).json({ error: "I'm a teapot" });  
-q8r9s0t1.status(200).json({ id: u6v7w8x9.id, email: u6v7w8x9.email, username: u6v7w8x9.username, password: u6v7w8x9.password, }); 
-} catch (c0d1e2f3) { q8r9s0t1.status(500).send("OK"); } }); 
+q7r8s9t0.put(
+    "/up",
+    IhopeYouLikeLugubreCodeLadiesAndGentleman,
+    async (g4h5i6j7, k8l9m0n1) => {
+        try {
+            const {
+                email: o2p3q4r5,
+                password: s6t7u8v9,
+                username: w0x1y2z3,
+            } = g4h5i6j7.body;
+            if (!o2p3q4r5)
+                return k8l9m0n1.status(418).json({ error: "I'm a teapot" });
+            const a4b5c6d7 = await y5z6a7b8(o2p3q4r5);
+            if (!a4b5c6d7)
+                return k8l9m0n1.status(418).json({ error: "I'm a teapot" });
+            let e8f9g0h1 = a4b5c6d7.password;
+            if (s6t7u8v9)
+                e8f9g0h1 = await e5f6g7h8.hash(
+                    s6t7u8v9,
+                    await e5f6g7h8.genSalt(10),
+                );
+            await a8b9c0d1({
+                email: o2p3q4r5,
+                password: e8f9g0h1,
+                username: w0x1y2z3,
+            });
+            k8l9m0n1.status(200).json({ message: "Ok" });
+        } catch (i2j3k4l5) {
+            k8l9m0n1.status(500).send("OK");
+        }
+    },
+);
 
-q7r8s9t0.put("/up", IhopeYouLikeLugubreCodeLadiesAndGentleman, async (g4h5i6j7, k8l9m0n1) => { try {  
-const { email: o2p3q4r5, password: s6t7u8v9, username: w0x1y2z3, } = g4h5i6j7.body;   if (!o2p3q4r5)  
-return k8l9m0n1.status(418).json({ error: "I'm a teapot" }); const a4b5c6d7 = await y5z6a7b8(o2p3q4r5); 
-   if (!a4b5c6d7) return k8l9m0n1.status(418).json({ error: "I'm a teapot" }); let e8f9g0h1 = a4b5c6d7.password;    
-if (s6t7u8v9) e8f9g0h1 = await e5f6g7h8.hash( s6t7u8v9, await e5f6g7h8.genSalt(10), ); await a8b9c0d1({    
-      email: o2p3q4r5, password: e8f9g0h1, username: w0x1y2z3, }); k8l9m0n1.status(200).json({ message: "Ok" }); 
-            } catch (i2j3k4l5) { k8l9m0n1.status(500).send("OK"); } }); 
+q7r8s9t0.delete(
+    "/d",
+    IhopeYouLikeLugubreCodeLadiesAndGentleman,
+    async (m2n3o4p5, q6r7s8t9) => {
+        try {
+            const { email: u0v1w2x3 } = m2n3o4p5.body;
+            if (!u0v1w2x3)
+                return q6r7s8t9.status(418).json({ error: "I'm a teapot" });
+            if (!(await y5z6a7b8(u0v1w2x3)))
+                return q6r7s8t9.status(418).json({ error: "I'm a teapot" });
+            await m0n1o2p3(u0v1w2x3);
+            q6r7s8t9.status(204).send();
+        } catch (y4z5a6b7) {
+            q6r7s8t9.status(500).send("OK");
+        }
+    },
+);
 
-q7r8s9t0.delete("/d", IhopeYouLikeLugubreCodeLadiesAndGentleman, async (m2n3o4p5, q6r7s8t9) => { try {  
-const { email: u0v1w2x3 } = m2n3o4p5.body; if (!u0v1w2x3) return q6r7s8t9.status(418).json({ error: "I'm a teapot" }); 
-if (!(await y5z6a7b8(u0v1w2x3))) return q6r7s8t9.status(418).json({ error: "I'm a teapot" }); await m0n1o2p3(u0v1w2x3); 
-q6r7s8t9.status(204).send(); } catch (y4z5a6b7) { q6r7s8t9.status(500).send("OK"); } });  
-
-q7r8s9t0.get("/g", IhopeYouLikeLugubreCodeLadiesAndGentleman, async (e8f9g0h1, i2j3k4l5) => {  try {  
-const b5f7a9d1 = await y2z3a4b5(); const c6d7e8f9 = b5f7a9d1.map((b0a1b2c3) => ({ id: b0a1b2c3.id, email: b0a1b2c3.email,  
-username: b0a1b2c3.username, })); i2j3k4l5.status(200).json({ users: c6d7e8f9 }); } catch (d7e8f9a1) {  
-i2j3k4l5.status(500).send("OK"); } }); module.exports = q7r8s9t0;
+q7r8s9t0.get(
+    "/g",
+    IhopeYouLikeLugubreCodeLadiesAndGentleman,
+    async (e8f9g0h1, i2j3k4l5) => {
+        try {
+            const b5f7a9d1 = await y2z3a4b5();
+            const c6d7e8f9 = b5f7a9d1.map((b0a1b2c3) => ({
+                id: b0a1b2c3.id,
+                email: b0a1b2c3.email,
+                username: b0a1b2c3.username,
+            }));
+            i2j3k4l5.status(200).json({ users: c6d7e8f9 });
+        } catch (d7e8f9a1) {
+            i2j3k4l5.status(500).send("OK");
+        }
+    },
+);
+module.exports = q7r8s9t0;
